b85c0ecf30674b9ec5180101c523453d7ec2a9f9
Jhonatas-Anthony <jhonatasanthony6@gmail.com>
2024-07-01 12:17:00 -0300

    ref: removing unused code

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,14 @@
-// import express from 'express'
+//const express = require('express')
 
-const express = require('express')
-
-// import { Router, Request, Response } from 'express';
-
-const { Router, Request} = express
+import express, { Request, Response } from 'express';
 
 const app = express();
+const port = 3333
 
-// const route = Router()
-
-//app.use(express.json())
-
-/* route.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'hello world with Typescript' })
-})
-
-app.use(route)
-
-
-app.listen(3333, () => 'server running on port 3333') */
-
-app.get('/', (req, res) => {
+app.get('/', (res: Response) => {
     res.send('Hello World!')
 })
 
-app.listen(3333, () => {
-    console.log(`Example app listening on port ${3333}`)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
